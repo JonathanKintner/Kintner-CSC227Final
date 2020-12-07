e8e1cbdd4230d79ac13a8039b32fb6d145335682
JonathanK <jpkintner@gmail.com>
2020-12-06 20:31:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var server = http.createServer(function (req, res) {
 	console.log("request was made: " + req.url);
