ef96c984071eb6e6a4e88bc463fb8165d656bace
JonathanK <jpkintner@gmail.com>
2020-12-06 20:11:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var express = require('express');
-var app = express();
+
 
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
@@ -14,7 +13,4 @@ var server = http.createServer(function (req, res) {
 	myRS.pipe(res);
 });
 
-
-app.listen(server_port, server_ip_address, function () {
-	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
-});
+server.listen(server_port,server_ip_address);
